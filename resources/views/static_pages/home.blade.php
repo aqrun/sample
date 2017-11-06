fed5690feba44d978f4f90a19fdfb1f290e2d9f1
Aqrun <aqruna@gmail.com>
2017-11-06 16:30:18 +0000

    Finish layouts and routes

---
+++ resources/views/static_pages/home.blade.php
@@ -0,0 +1,18 @@
+@extends('layouts.default')
+
+@section('content')
+    <div class="jumbotron">
+        <h1>Hello laravel</h1>
+        <p class="lead">
+            你现在所看到的是 <a href="https://fsdhub.com/books/laravel-essential-training-5.1">Laravel 
+        </p>
+        <p>
+            一切，将从这里开始。
+        </p>
+        <p>
+            <a class="btn btn-lg btn-success" href="#" role="button">现在注册</a>
+        </p>
+
+    </div>
+
+@stop
