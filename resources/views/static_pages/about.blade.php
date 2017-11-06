4da42b546bcb04cbabd215389e31f863afa86bbd
Aqrun <aqruna@gmail.com>
2017-11-06 15:08:10 +0000

    Finis static pages

---
+++ resources/views/static_pages/about.blade.php
@@ -0,0 +1,6 @@
+@extends('layout.default')
+@section('title', 'About')
+@section('content')
+    <h1>about page</h1>
+
+@stop
---
+++ resources/views/static_pages/home.blade.php
@@ -0,0 +1,6 @@
+@extends('layouts.default')
+@section('title', 'Home')
+
+@section('content')
+    <h1>main page</h1>
+@stop
